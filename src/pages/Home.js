795535e9de76b218823b253f43421412a7ca0d63
raisasalsabily <raisa.salsabil@mail.ugm.ac.id>
2022-12-27 19:12:47 +0700

    feat(HelpBox): add HelpBox component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,6 +3,7 @@ import React from "react"
 import Navbar from "./../components/navbar/Navbar.jsx"
 import Footer from "./../components/footer/Footer.jsx"
 import HelpButton from "../components/button/HelpButton.jsx"
+import HelpBox from "../components/box/HelpBox.jsx"
 
 const Home = () => {
   return (
@@ -10,7 +11,12 @@ const Home = () => {
       <div className="font-poppins">
         <Navbar />
         <div className="h-[400px] flex items-center justify-center">BLANK</div>
-        <HelpButton />
+
+        {/* helpbox */}
+        <div className="flex justify-center items-center m-10">
+          <HelpBox className="" />
+        </div>
+
         <Footer />
       </div>
     </>
