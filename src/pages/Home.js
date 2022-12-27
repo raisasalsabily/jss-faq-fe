78d96106c727d1b4646957091cbaa0e62bfb4c50
raisasalsabily <raisa.salsabil@mail.ugm.ac.id>
2022-12-27 14:13:49 +0700

    feat(HelpButton): add basic HelpButton

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,6 +2,7 @@ import React from "react"
 
 import Navbar from "./../components/navbar/Navbar.jsx"
 import Footer from "./../components/footer/Footer.jsx"
+import HelpButton from "../components/button/HelpButton.jsx"
 
 const Home = () => {
   return (
@@ -9,6 +10,7 @@ const Home = () => {
       <div className="font-poppins">
         <Navbar />
         <div className="h-[400px] flex items-center justify-center">BLANK</div>
+        <HelpButton />
         <Footer />
       </div>
     </>
