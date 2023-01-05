005faba611b3cfe2b65ee7bdec978a9608487da0
Ipung Sonia F <79226247+ipungsonia@users.noreply.github.com>
2023-01-05 10:04:35 +0700

    feat(backbtn): add back button to homepage

--- src/pages/Testing.js
+++ src/pages/Testing.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import TagBox from '../components/box/TagBox'
+import BackBtn from '../components/button/BackBtn'
 
 export const Testing = () => {
   return (
     <div>
         <h1>Dummy Page just for Testing components</h1>
         <TagBox/>
+        <BackBtn/>
     </div>
   )
 }
