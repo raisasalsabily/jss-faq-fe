4daa339469ae357faf4563c67dfef41f972702be
Ipung Sonia F <79226247+ipungsonia@users.noreply.github.com>
2023-01-05 10:52:37 +0700

    feat(faqbtn): add delete and edit buttons

--- src/pages/Testing.js
+++ src/pages/Testing.js
@@ -2,6 +2,8 @@ import React from 'react'
 import TagBox from '../components/box/TagBox'
 import AddDataBtn from '../components/button/AddDataBtn'
 import BackBtn from '../components/button/BackBtn'
+import DeleteBtn from '../components/button/DeleteBtn'
+import EditBtn from '../components/button/EditBtn'
 import FilterBtn from '../components/button/FilterBtn'
 import SaveBtn from '../components/button/SaveBtn'
 import ReadTime from '../components/icon/ReadTime'
@@ -16,6 +18,8 @@ export const Testing = () => {
         <SaveBtn/>
         <AddDataBtn/>
         <FilterBtn/>
+        <DeleteBtn/>
+        <EditBtn/>
     </div>
   )
 }
