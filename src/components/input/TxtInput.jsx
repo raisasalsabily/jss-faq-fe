bef88f42a6168a24bb02616771b1a145e1c7333f
raisasalsabily <raisa.salsabil@mail.ugm.ac.id>
2023-01-05 13:28:45 +0700

    feat(input): add TxtInput and InputLabel components

---
+++ src/components/input/TxtInput.jsx
@@ -0,0 +1,21 @@
+import React from "react"
+
+function TxtInput(props) {
+  return (
+    <input
+      type="text"
+      className={
+        "form-control block w-full px-3 py-1.5 text-b-md font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-neutral-200 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" +
+        props.className
+      }
+      id={props.id}
+      placeholder={props.placeholder}
+    />
+  )
+}
+
+TxtInput.defaultProps = {
+  placeholder: "Insert placeholder",
+}
+
+export default TxtInput
