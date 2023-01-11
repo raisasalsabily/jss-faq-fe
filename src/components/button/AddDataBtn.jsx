6b962f652ceaa6c1d16e0529a6bc847cd78433c7
Ipung Sonia F <79226247+ipungsonia@users.noreply.github.com>
2023-01-11 13:41:25 +0700

    feature(header): add props Content Header

--- src/components/button/AddDataBtn.jsx
+++ src/components/button/AddDataBtn.jsx
@@ -4,7 +4,7 @@ import { Icon } from '@iconify/react';
 function AddDataBtn() {
     return (  
         <button className='w-[150px] h-[40px] rounded-md font-poppins text-neutral-100 gap-1 flex justify-center items-center bg-teal-500'>
-            <Icon icon="material-symbols:add" />
+            <Icon icon="material-symbols:add" className='text-white'/>
             <p>Tambah Data</p>
         </button>
     );
--- src/components/searchbar/SearchBar.jsx
+++ src/components/searchbar/SearchBar.jsx
@@ -2,14 +2,14 @@ import React from "react"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons"
 
-function Searchbar() {
+function Searchbar(props) {
   return (
-    <form className="w-[640px] pt-2 relative mx-auto">
+    <form className={`w-[640px] pt-2 relative ${props.className}`}>
       <input
-        className="w-[640px] h-11 px-5 pr-16 border-2 border-neutral-300 rounded-full text-b-lg text-neutral-600 focus:outline-none focus:ring-teal-50 focus:border-teal-50"
+        className="w-full h-11 px-5 pr-16 border-2 border-neutral-300 rounded-full text-b-lg text-neutral-600 focus:outline-none focus:ring-teal-50 focus:border-teal-50"
         type="text"
         name="search"
-        placeholder="Telusuri..."
+        placeholder={props.placeholder}
         required
       />
       <button type="submit" className="absolute right-0 top-0 mt-5 mr-4 flex">
@@ -23,3 +23,7 @@ function Searchbar() {
 }
 
 export default Searchbar
+
+Searchbar.defaultProps = {
+  placeholder: "Telusuri...",
+};
