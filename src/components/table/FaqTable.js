6d38e0cf4342751d338c704eff1588b5cc2ee696
Hafizha Ulinnuha Ahmad <hafizhaua@gmail.com>
2023-01-13 11:13:46 +0800

    fix: component styling and reusability

--- src/components/table/FaqTable.js
+++ src/components/table/FaqTable.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import useTable from "../../hooks/useTable";
 import DeleteBtn from "../button/DeleteBtn";
 import EditBtn from "../button/EditBtn";
@@ -8,23 +8,36 @@ import TablePagination from "./TablePagination";
 export default function FaqTable({ className = "", data, rowsPerPage }) {
     const [page, setPage] = useState(1);
     const { slice, range } = useTable(data, page, rowsPerPage);
+    const lstProp = Object.keys(data[0]);
 
     return (
-        <>
-            <div className={`${className} drop-shadow-lg`}>
+        <div className="flex flex-col items-end">
+            <div className={`${className} drop-shadow-lg w-full`}>
                 <table className="table-auto rounded-sm m-0 w-full">
-                    <thead className="px-12 py-4 bg-neutral-100">
+                    <thead className="px-12 py-4 bg-[#f4f5f5]">
                         <tr className="px-12 py-4">
-                            <th className="rounded-tl-lg w-2/12 uppercase pl-10 pr-1 py-4 text-left text-neutral-600 text-b-sm font-bold">
-                                ID
+                            <th className="w-2/12 rounded-tl-lg uppercase pl-10 py-4 text-left text-neutral-600 text-b-sm font-bold">
+                                {lstProp[0]}
                             </th>
-                            <th className="w-6/12 uppercase px-4 py-4 text-left text-neutral-600 text-b-sm font-bold">
+                            {lstProp.map((prop, i) => {
+                                if (i > 0) {
+                                    return (
+                                        <th
+                                            key={prop}
+                                            className="uppercase px-4 py-4 text-left text-neutral-600 text-b-sm font-bold"
+                                        >
+                                            {prop}
+                                        </th>
+                                    );
+                                } else return;
+                            })}
+                            {/* <th className="uppercase px-4 py-4 text-left text-neutral-600 text-b-sm font-bold">
                                 Pertanyaan
                             </th>
-                            <th className="w-2/12 uppercase px-4 py-4 text-left text-neutral-600 text-b-sm font-bold">
+                            <th className="uppercase px-4 py-4 text-left text-neutral-600 text-b-sm font-bold">
                                 Kategori
-                            </th>
-                            <th className="rounded-tr-lg pr-10 pl-4 uppercase py-4 text-left text-neutral-600 text-b-sm font-bold">
+                            </th> */}
+                            <th className="w-2/12 rounded-tr-lg pr-10 pl-4 uppercase py-4 text-left text-neutral-600 text-b-sm font-bold">
                                 Ubah
                             </th>
                         </tr>
@@ -33,18 +46,23 @@ export default function FaqTable({ className = "", data, rowsPerPage }) {
                         {slice.map((data) => {
                             return (
                                 <tr
-                                    key={data.id}
+                                    key={data[lstProp[0]]}
                                     className="last:[&>td]:last:rounded-br-lg  first:[&>td]:last:rounded-bl-lg"
                                 >
-                                    <td className="w-16 pl-10 pr-1 py-4 text-left text-neutral-900 text-b-sm tabular-nums">
-                                        {data.id}
+                                    <td className="pl-10 pr-1 py-4 text-left text-neutral-900 text-b-sm tabular-nums">
+                                        {data[lstProp[0]]}
                                     </td>
-                                    <td className="text-left px-4 py-4 text-neutral-900 text-b-sm">
-                                        {data.title}
-                                    </td>
-                                    <td className="text-left px-4 py-4 text-neutral-900 text-b-sm">
-                                        {data.category}
-                                    </td>
+
+                                    {lstProp.map((prop, i) => {
+                                        console.log(prop);
+                                        if (i > 0) {
+                                            return (
+                                                <td className="text-left px-4 py-4 text-neutral-900 text-b-sm">
+                                                    {data[prop]}
+                                                </td>
+                                            );
+                                        } else return;
+                                    })}
                                     <td className="text-left pl-4 pr-10 text-neutral-900 text-b-sm">
                                         <PopConfirm
                                             onCancel={() => alert("gagal")}
@@ -71,6 +89,6 @@ export default function FaqTable({ className = "", data, rowsPerPage }) {
                 setPage={setPage}
                 page={page}
             />
-        </>
+        </div>
     );
 }
