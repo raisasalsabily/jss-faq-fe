137c586a97c70126f53cef360a9c5f1e4a8cdc4e
raisasalsabily <raisa.salsabil@mail.ugm.ac.id>
2023-01-15 04:45:57 +0700

    feat: fetch faqs data for each categories

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,45 +6,48 @@ import Searchbar from "../components/searchbar/SearchBar.jsx"
 import CategorySidebar from "../components/Category/CategorySidebar.jsx"
 import HelpBox from "../components/box/HelpBox.jsx"
 import FaqList from "../components/collapse/FaqList.js"
+import { useLocation } from "react-router-dom"
+import { faColonSign } from "@fortawesome/free-solid-svg-icons"
 
 const Home = () => {
   const faqData = [
     {
       id: 1,
-      title: "Bagaimana cara menginstall aplikasi JSS di android/iphone saya?",
-      article:
+      question:
+        "Bagaimana cara menginstall aplikasi JSS di android/iphone saya?",
+      answer:
         "Website layanan kota adalah sebuah platform yang dibuat oleh pemerintah kota atau daerah untuk memberikan informasi dan layanan kepada masyarakat yang tinggal di kota atau daerah tersebut. Tujuan utama dari website ini adalah untuk mempermudah akses masyarakat ke informasi dan layanan yang ditawarkan oleh pemerintah, sehingga masyarakat tidak perlu datang ke kantor pemerintah secara langsung untuk mendapatkan informasi atau mengajukan permohonan layanan.",
       category: "Akun",
       tag: ["JSS", "Aplikasi", "Install"],
     },
     {
       id: 2,
-      title: "Bagaimana cara mendaftar JSS menggunakan WhatsApp?",
-      article:
+      question: "Bagaimana cara mendaftar JSS menggunakan WhatsApp?",
+      answer:
         "Website layanan kota juga bisa menjadi sumber informasi yang berguna bagi wisatawan yang akan berkunjung ke kota tersebut, karena website ini biasanya menyediakan informasi tentang tempat-tempat wisata yang ada di kota tersebut, akomodasi, dan sebagainya.",
       category: "Akun",
       tag: ["JSS", "Aplikasi", "Install"],
     },
     {
       id: 3,
-      title: "Bagaimana cara mendaftar JSS menggunakan email?",
-      article:
+      question: "Bagaimana cara mendaftar JSS menggunakan email?",
+      answer:
         "Secara keseluruhan, website layanan kota merupakan salah satu cara yang efektif bagi pemerintah untuk memperluas akses masyarakat ke informasi dan layanan yang ditawarkan, serta mempermudah proses pengajuan permohonan layanan oleh masyarakat.",
       category: "Akun",
       tag: ["JSS", "Aplikasi", "Install"],
     },
     {
       id: 4,
-      title: "Link aktivasi tidak terkirim ke WhatsApp saya",
-      article:
+      question: "Link aktivasi tidak terkirim ke WhatsApp saya",
+      answer:
         "Anda dapat menghubungi layanan pelanggan untuk meminta bantuan mengembalikan password. Pastikan untuk memberikan informasi yang cukup seperti nama pengguna",
       category: "Akun",
       tag: ["JSS", "Aplikasi", "Install"],
     },
     {
       id: 5,
-      title: "Username dan/atau password saya dinyatakan salah",
-      article:
+      question: "Username dan/atau password saya dinyatakan salah",
+      answer:
         "Anda dapat menghubungi layanan pelanggan untuk meminta bantuan mengembalikan password. Pastikan untuk memberikan informasi yang cukup seperti nama pengguna",
       category: "Akun",
       tag: ["JSS", "Aplikasi", "Install"],
@@ -52,6 +55,9 @@ const Home = () => {
   ]
 
   const [cats, setCats] = useState([]) // state to fetch category sidebar items
+  const [faqs, setFaqs] = useState([]) // state to fetch faqs
+  const { search } = useLocation()
+  // console.log(location)
 
   useEffect(() => {
     const getCats = async () => {
@@ -60,6 +66,14 @@ const Home = () => {
     }
     getCats()
   }, [])
+
+  useEffect(() => {
+    const fetchFaqs = async () => {
+      const res = await axios.get("http://localhost:5000/api/posts" + search)
+      setFaqs(res.data)
+    }
+    fetchFaqs()
+  }, [search])
 
   return (
     <>
@@ -86,10 +100,9 @@ const Home = () => {
               <aside>
                 <CategorySidebar cats={cats} />
               </aside>
-
               {/* QuestionList */}
               <main className="p-10">
-                <FaqList data={faqData} category={faqData[0].category} />
+                <FaqList data={faqs} category={faqs[0].category} />
               </main>
             </div>
           </div>
