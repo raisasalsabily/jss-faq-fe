71ef197ebc3d14db57dc440e2b76aebe10589875
Ipung Sonia F <79226247+ipungsonia@users.noreply.github.com>
2023-01-20 15:38:56 +0700

    fix: responsive style for components

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -5,24 +5,24 @@ export default function Footer() {
   return (
     <footer
       id="footer"
-      className="h-[500px] lg:h-[250px] box-border footerbg flex w-full items-center justify-center text-white text-left"
+      className="h-[559px] lg:h-[250px] box-border footerbg flex w-full items-center justify-center text-white text-left"
     >
       <div className="mx-2 lg:mx-[130px]">
         {/* -------------- FOOTER TOP START -------------- */}
-        <div id="footer-top" className="flex flex-col lg:flex-row">
+        <div id="footer-top" className="flex flex-col gap-4 lg:flex-row">
           <div id="pelajari" className="lg:basis-[300px] px-2">
-            <h5 className="font-semibold text-b-lg lg:text-[18px] mb-4">
+            <h5 className="font-semibold text-b-lg lg:text-[18px] mb-2">
               Pelajari
             </h5>
-            <p className="text-b-md lg:text-b-md py-1">
+            <p className="text-b-md lg:text-b-md">
               Syarat dan Ketentuan
             </p>
-            <p className="text-b-md lg:text-b-md py-1">
+            <p className="text-b-md lg:text-b-md">
               Kebijakan Privasi Data
             </p>
           </div>
           <div id="apa-jss" className="lg:basis-[450px] px-2">
-            <h5 className="font-semibold text-b-lg lg:text-[18px] mb-4">
+            <h5 className="font-semibold text-b-lg lg:text-[18px] mb-2">
               Apa sih JSS itu ?
             </h5>
             <p className="text-b-md lg:text-b-md">
@@ -32,7 +32,7 @@ export default function Footer() {
             </p>
           </div>
           <div id="satu-pintu" className="lg:basis-3/12 px-2">
-            <h5 className="font-semibold text-b-lg lg:text-[18px] mb-4">
+            <h5 className="font-semibold text-b-lg lg:text-[18px] mb-2">
               Satu Pintu
             </h5>
             <p className="text-b-md lg:text-b-md">
@@ -42,7 +42,7 @@ export default function Footer() {
           </div>
           <div
             id="download-app"
-            className="lg:basis-auto flex flex-row lg:flex-col gap-2 px-2 justify-center items-center"
+            className="lg:basis-auto flex flex-row lg:flex-col gap-2 p-2 justify-center items-center"
           >
             <img
               src="https://jss.jogjakota.go.id/v4/img/ios.png"
