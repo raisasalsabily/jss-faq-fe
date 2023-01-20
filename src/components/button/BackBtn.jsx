db4cc833fd81d1d35d67cfd82f55aaf01539056d
Ipung Sonia F <79226247+ipungsonia@users.noreply.github.com>
2023-01-20 16:36:25 +0700

    fix: style back button

--- src/components/button/BackBtn.jsx
+++ src/components/button/BackBtn.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 function BackBtn() {
     return (
         <Link to="/">
-            <button className='font-poppins text-teal-500 py-4 md:text-b-lg text-b-sm'>
+            <button className='font-poppins text-teal-500 my-4 md:text-b-lg text-b-sm hover:drop-shadow-lg hover:-translate-y-1 transition'>
                 ← Kembali
             </button>
         </Link>        
