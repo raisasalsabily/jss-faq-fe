20e9c20e8f6b4940e35561e711924fc291e061bb
Ipung Sonia F <79226247+ipungsonia@users.noreply.github.com>
2023-01-20 15:40:40 +0700

    fix: responsive style for homepage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,53 +2,14 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import Navbar from "./../components/navbar/Navbar.jsx";
 import Footer from "./../components/footer/Footer.jsx";
-import Searchbar from "../components/searchbar/SearchBar.jsx";
 import CategorySidebar from "../components/Category/CategorySidebar.jsx";
 import HelpBox from "../components/box/HelpBox.jsx";
+import JSSLiveChat from '../components/icon/JSSLiveChat';
 import FaqList from "../components/collapse/FaqList.js";
 import { useLocation } from "react-router-dom";
-import { faColonSign } from "@fortawesome/free-solid-svg-icons";
+import SearchBox from "../components/box/SearchBox.jsx";
 
 const Home = () => {
-    const faqData = [
-        {
-            id: 1,
-            question:
-                "Bagaimana cara menginstall aplikasi JSS di android/iphone saya?",
-            answer: "Website layanan kota adalah sebuah platform yang dibuat oleh pemerintah kota atau daerah untuk memberikan informasi dan layanan kepada masyarakat yang tinggal di kota atau daerah tersebut. Tujuan utama dari website ini adalah untuk mempermudah akses masyarakat ke informasi dan layanan yang ditawarkan oleh pemerintah, sehingga masyarakat tidak perlu datang ke kantor pemerintah secara langsung untuk mendapatkan informasi atau mengajukan permohonan layanan.",
-            category: "Akun",
-            tag: ["JSS", "Aplikasi", "Install"],
-        },
-        {
-            id: 2,
-            question: "Bagaimana cara mendaftar JSS menggunakan WhatsApp?",
-            answer: "Website layanan kota juga bisa menjadi sumber informasi yang berguna bagi wisatawan yang akan berkunjung ke kota tersebut, karena website ini biasanya menyediakan informasi tentang tempat-tempat wisata yang ada di kota tersebut, akomodasi, dan sebagainya.",
-            category: "Akun",
-            tag: ["JSS", "Aplikasi", "Install"],
-        },
-        {
-            id: 3,
-            question: "Bagaimana cara mendaftar JSS menggunakan email?",
-            answer: "Secara keseluruhan, website layanan kota merupakan salah satu cara yang efektif bagi pemerintah untuk memperluas akses masyarakat ke informasi dan layanan yang ditawarkan, serta mempermudah proses pengajuan permohonan layanan oleh masyarakat.",
-            category: "Akun",
-            tag: ["JSS", "Aplikasi", "Install"],
-        },
-        {
-            id: 4,
-            question: "Link aktivasi tidak terkirim ke WhatsApp saya",
-            answer: "Anda dapat menghubungi layanan pelanggan untuk meminta bantuan mengembalikan password. Pastikan untuk memberikan informasi yang cukup seperti nama pengguna",
-            category: "Akun",
-            tag: ["JSS", "Aplikasi", "Install"],
-        },
-        {
-            id: 5,
-            question: "Username dan/atau password saya dinyatakan salah",
-            answer: "Anda dapat menghubungi layanan pelanggan untuk meminta bantuan mengembalikan password. Pastikan untuk memberikan informasi yang cukup seperti nama pengguna",
-            category: "Akun",
-            tag: ["JSS", "Aplikasi", "Install"],
-        },
-    ];
-
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState(false);
 
@@ -116,21 +77,12 @@ const Home = () => {
                     className="flex justify-center items-center min-h-screen"
                 >
                     <div className="max-w-7xl w-11/12 mt-32 flex flex-col justify-center items-center">
-                        {/* Title */}
-                        <div id="title">
-                            <h1 className="mb-2 text-h-lg font-bold">
-                                Halo, ada yang dapat kami bantu?
-                            </h1>
-                        </div>
-                        {/* searchbar */}
-                        <div>
-                            <Searchbar />
-                        </div>
+                        <SearchBox/>
 
                         {/* sidebar and question list */}
                         <div className="min-w-full my-20 border-t border-neutral-200 flex sticky top-[10vh]">
                             {/* CategorySidebar */}
-                            <aside className="w-4/12">
+                            <aside className="hidden md:flex w-4/12">
                                 <CategorySidebar cats={cats} />
                             </aside>
                             {/* QuestionList */}
@@ -146,11 +98,11 @@ const Home = () => {
                     </div>
                 </div>
                 {/* end - container */}
-
                 {/* helpbox */}
                 <div id="helpbox">
                     <HelpBox />
                 </div>
+                <JSSLiveChat/>
 
                 <Footer />
             </div>
