c0681d5cc4eeebe661315e1315dcca1c487826b4
raisasalsabily <raisa.salsabil@mail.ugm.ac.id>
2023-01-28 23:52:19 +0700

    feat(search): integrate search feature

--- src/components/search/SearchList.jsx
+++ src/components/search/SearchList.jsx
@@ -4,11 +4,10 @@ import SearchItem from "./SearchItem"
 export default function SearchList({ data, category }) {
   return (
     <div>
-      {category && <h4 className="pt-5 text-h-sm font-bold">{category}</h4>}
-
+      {/* {category && <h4 className="pt-5 text-h-sm font-bold">{category}</h4>} */}
       <div className={category && "sm:ml-4"}>
-        {data.map(({ title, article }) => (
-          <SearchItem title={title} content={article} />
+        {data.map(({ question, answer }) => (
+          <SearchItem question={question} answer={answer} />
         ))}
       </div>
     </div>
