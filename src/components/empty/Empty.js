f57996cd7f61a51d5389a047f1cae9101ed91149
Hafizha Ulinnuha Ahmad <hafizhaua@gmail.com>
2023-01-30 17:36:49 +0700

    feat: add empty component

---
+++ src/components/empty/Empty.js
@@ -0,0 +1,13 @@
+import React from "react";
+import EmptyIcon from "../../assets/images/empty.png";
+
+export default function Empty({ message = "Tidak ada data" }) {
+    return (
+        <div className="w-full h-full flex flex-col justify-center items-center">
+            <img src={EmptyIcon} />
+            <h4 className="mt-4 text-neutral-600 font-medium text-b-lg">
+                {message}
+            </h4>
+        </div>
+    );
+}
