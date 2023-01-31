73bc98019693abebc9d3e11c02c341979d8c48db
raisasalsabily <raisa.salsabil@mail.ugm.ac.id>
2023-01-31 17:22:45 +0700

    fix: re-enable delete faq feature

--- src/pages/dashboard/FaqDashboard.js
+++ src/pages/dashboard/FaqDashboard.js
@@ -1,120 +1,123 @@
-import axios from "axios";
-import React, { useEffect, useState } from "react";
+import axios from "axios"
+import React, { useEffect, useState } from "react"
 import {
-    faMagnifyingGlass,
-    faExternalLinkAlt,
-} from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { Link } from "react-router-dom";
+  faMagnifyingGlass,
+  faExternalLinkAlt,
+} from "@fortawesome/free-solid-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { Link } from "react-router-dom"
 
-import DashboardLayout from "../../components/layout/DashboardLayout";
-import DefaultLayout from "../../components/layout/DefaultLayout";
-import DashboardTitle from "../../components/dashboard/DashboardTitle";
-import FaqTable from "../../components/table/FaqTable";
-import AddDataBtn from "../../components/button/AddDataBtn";
+import DashboardLayout from "../../components/layout/DashboardLayout"
+import DefaultLayout from "../../components/layout/DefaultLayout"
+import DashboardTitle from "../../components/dashboard/DashboardTitle"
+import FaqTable from "../../components/table/FaqTable"
+import AddDataBtn from "../../components/button/AddDataBtn"
 
 const FaqDashboard = () => {
-    const [faqs, setFaqs] = useState([]);
-    const [dataTable, setDataTable] = useState([]);
+  const [faqs, setFaqs] = useState([])
+  const [dataTable, setDataTable] = useState([])
 
-    const generateLink = (data) => {
-        if (data._id)
-            data.url = (
-                <Link to={`/post/${data._id}`} target="_blank">
-                    <FontAwesomeIcon
-                        icon={faExternalLinkAlt}
-                        className="text-teal-300 hover:text-teal-900 transition-colors"
-                    />
-                </Link>
-            );
-        return data;
-    };
+  const generateLink = (data) => {
+    if (data._id)
+      data.url = (
+        <Link to={`/post/${data._id}`} target="_blank">
+          <FontAwesomeIcon
+            icon={faExternalLinkAlt}
+            className="text-teal-300 hover:text-teal-900 transition-colors"
+          />
+        </Link>
+      )
+    return data
+  }
 
-    const getFaqs = async () => {
-        try {
-            const res = await axios.get(
-                "http://localhost:5000/api/posts/?table=true"
-            );
-            let data = res.data;
-            data = data.map(generateLink);
-            setFaqs(data);
-            setDataTable(data);
-        } catch (err) {
-            console.log(err);
-        }
-    };
+  const getFaqs = async () => {
+    try {
+      const res = await axios.get("http://localhost:5000/api/posts/?table=true")
+      let data = res.data
+      data = data.map(generateLink)
+      setFaqs(data)
+      setDataTable(data)
+    } catch (err) {
+      console.log(err)
+    }
+  }
 
-    useEffect(() => {
-        getFaqs();
-    }, []);
+  const deleteFaq = async (id) => {
+    try {
+      await axios.delete("http://localhost:5000/api/posts/" + id)
+      getFaqs()
+    } catch (error) {
+      console.log(error)
+    }
+  }
 
-    const handleSearch = (e) => {
-        const key = e.target.value.toLowerCase();
+  useEffect(() => {
+    getFaqs()
+  }, [])
 
-        const searchRes = faqs.filter((obj) => {
-            let isInclude = false;
-            for (const prop in obj) {
-                if (obj[prop].toString().toLowerCase().includes(key))
-                    isInclude = true;
-            }
-            return isInclude;
-        });
+  const handleSearch = (e) => {
+    const key = e.target.value.toLowerCase()
 
-        setDataTable(searchRes);
-    };
+    const searchRes = faqs.filter((obj) => {
+      let isInclude = false
+      for (const prop in obj) {
+        if (obj[prop].toString().toLowerCase().includes(key)) isInclude = true
+      }
+      return isInclude
+    })
 
-    return (
-        <DefaultLayout>
-            <DashboardLayout>
-                <div className="flex justify-between">
-                    <DashboardTitle
-                        id="index-data-title"
-                        title="FAQ"
-                        subTitle={
-                            dataTable
-                                ? dataTable.length + " data ditemukan"
-                                : ""
-                        }
-                    />
-                    <Link to="create">
-                        <AddDataBtn />
-                    </Link>
-                </div>
+    setDataTable(searchRes)
+  }
 
-                <div className={`w-96 relative self-end`}>
-                    <input
-                        className="w-full py-1 px-4 border-2 border-neutral-300 rounded-full text-b-md text-neutral-600 focus:outline-none focus:ring-teal-50 focus:border-teal-50"
-                        type="text"
-                        name="search"
-                        placeholder="Telusuri..."
-                        onChange={handleSearch}
-                        required
-                    />
-                    <button className="absolute right-0 top-2 mr-4 flex">
-                        <FontAwesomeIcon
-                            icon={faMagnifyingGlass}
-                            className="text-neutral-600"
-                        />
-                    </button>
-                </div>
+  return (
+    <DefaultLayout>
+      <DashboardLayout>
+        <div className="flex justify-between">
+          <DashboardTitle
+            id="index-data-title"
+            title="FAQ"
+            subTitle={dataTable ? dataTable.length + " data ditemukan" : ""}
+          />
+          <Link to="create">
+            <AddDataBtn />
+          </Link>
+        </div>
 
-                {/* Table */}
-                {dataTable && (
-                    <div data-aos="fade">
-                        <FaqTable
-                            data={dataTable}
-                            rowsPerPage="5"
-                            lstProp={[
-                                { attribute: "question", label: "Pertanyaan" },
-                                { attribute: "category", label: "Kategori" },
-                                { attribute: "url", label: "Laman" },
-                            ]}
-                        />
-                    </div>
-                )}
-            </DashboardLayout>
-        </DefaultLayout>
-    );
-};
+        <div className={`w-96 relative self-end`}>
+          <input
+            className="w-full py-1 px-4 border-2 border-neutral-300 rounded-full text-b-md text-neutral-600 focus:outline-none focus:ring-teal-50 focus:border-teal-50"
+            type="text"
+            name="search"
+            placeholder="Telusuri..."
+            onChange={handleSearch}
+            required
+          />
+          <button className="absolute right-0 top-2 mr-4 flex">
+            <FontAwesomeIcon
+              icon={faMagnifyingGlass}
+              className="text-neutral-600"
+            />
+          </button>
+        </div>
 
-export default FaqDashboard;
+        {/* Table */}
+        {dataTable && (
+          <div data-aos="fade">
+            <FaqTable
+              data={dataTable}
+              rowsPerPage="5"
+              lstProp={[
+                { attribute: "question", label: "Pertanyaan" },
+                { attribute: "category", label: "Kategori" },
+                { attribute: "url", label: "Laman" },
+              ]}
+              onDelete={deleteFaq}
+            />
+          </div>
+        )}
+      </DashboardLayout>
+    </DefaultLayout>
+  )
+}
+
+export default FaqDashboard
