d0d98f284263a2d5276ba91c9d1e1a4f25e3e2cc
raisasalsabily <raisa.salsabil@mail.ugm.ac.id>
2023-02-03 06:39:57 +0700

    feat: add show faq

--- src/pages/dashboard/create/CreateFaq.js
+++ src/pages/dashboard/create/CreateFaq.js
@@ -1,211 +1,224 @@
-import axios from "axios";
-import React, { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
-import SaveBtn from "../../../components/button/SaveBtn";
-import DashboardTitle from "../../../components/dashboard/DashboardTitle";
-import DropSearch from "../../../components/input/DropSearch";
-import InputLabel from "../../../components/input/InputLabel";
-import MultiInput from "../../../components/input/MultiInput";
-import QuillEditor from "../../../components/input/QuillEditor";
-import TxtInput from "../../../components/input/TxtInput";
-import DashboardLayout from "../../../components/layout/DashboardLayout";
-import DefaultLayout from "../../../components/layout/DefaultLayout";
-import imageTest from "../../../assets/images/batik.svg";
-import SlugButton from "../../../components/button/SlugButton";
+import axios from "axios"
+import React, { useEffect, useState } from "react"
+import { useNavigate } from "react-router-dom"
+import SaveBtn from "../../../components/button/SaveBtn"
+import DashboardTitle from "../../../components/dashboard/DashboardTitle"
+import DropSearch from "../../../components/input/DropSearch"
+import InputLabel from "../../../components/input/InputLabel"
+import MultiInput from "../../../components/input/MultiInput"
+import QuillEditor from "../../../components/input/QuillEditor"
+import TxtInput from "../../../components/input/TxtInput"
+import DashboardLayout from "../../../components/layout/DashboardLayout"
+import DefaultLayout from "../../../components/layout/DefaultLayout"
+import imageTest from "../../../assets/images/batik.svg"
+import SlugButton from "../../../components/button/SlugButton"
 
 export default function CreateFaq() {
-    const navigate = useNavigate();
+  const navigate = useNavigate()
 
-    const [loading, setLoading] = useState(false);
-    const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState(false)
 
-    const [question, setQuestion] = useState("");
-    const [slug, setSlug] = useState("");
-    const [category, setCategory] = useState("");
+  const [question, setQuestion] = useState("")
+  const [slug, setSlug] = useState("")
+  const [category, setCategory] = useState("")
 
-    const [cats, setCats] = useState([]); // state to fetch category
-    const [dropInput, setDropInput] = useState(""); // input value for category dropdown
-    const [dropSelected, setDropSelected] = useState(""); // selected value of category dropdown
-    const [dropOpen, setDropOpen] = useState(""); //dropdown open or close
+  const [cats, setCats] = useState([]) // state to fetch category
+  const [dropInput, setDropInput] = useState("") // input value for category dropdown
+  const [dropSelected, setDropSelected] = useState("") // selected value of category dropdown
+  const [dropOpen, setDropOpen] = useState("") //dropdown open or close
 
-    const [answer, setAnswer] = useState([]); // state untuk TextEditor
-    const [files, setFiles] = useState([]); // state untuk files TextEditor
+  const [answer, setAnswer] = useState([]) // state untuk TextEditor
+  const [files, setFiles] = useState([]) // state untuk files TextEditor
 
-    const [tag, setTag] = useState([]);
-    const [errors, setErrors] = useState({});
+  const [tag, setTag] = useState([])
+  const [show, setIsShow] = useState(false)
+  const [errors, setErrors] = useState({})
 
-    const changeHandler = (name, value) => {
-        if (name === "tag") {
-            setTag(value);
-            if (value.length > 0 && errors.tag) {
-                setError((prev) => {
-                    const prevErrors = { ...prev };
-                    delete prevErrors.tag;
-                    return prevErrors;
-                });
-            }
-        }
-    };
+  const changeHandler = (name, value) => {
+    if (name === "tag") {
+      setTag(value)
+      if (value.length > 0 && errors.tag) {
+        setError((prev) => {
+          const prevErrors = { ...prev }
+          delete prevErrors.tag
+          return prevErrors
+        })
+      }
+    }
+  }
 
-    const onEditorChange = (value) => {
-        setAnswer(value);
-        // console.log(value)
-    };
+  const onEditorChange = (value) => {
+    setAnswer(value)
+    // console.log(value)
+  }
 
-    const onFilesChange = (files) => {
-        setFiles(files);
-    };
+  const onFilesChange = (files) => {
+    setFiles(files)
+  }
 
-    const getCats = async () => {
-        setLoading(true);
-        try {
-            const res = await axios.get(
-                "http://localhost:5000/api/categories",
-                {
-                    headers: {
-                        Accept: "application/json",
-                    },
-                }
-            );
-            setCats(res.data);
-        } catch (error) {
-            setError(error);
-            // console.log(error)
-        }
-        setLoading(false);
-    };
+  const getCats = async () => {
+    setLoading(true)
+    try {
+      const res = await axios.get("http://localhost:5000/api/categories", {
+        headers: {
+          Accept: "application/json",
+        },
+      })
+      setCats(res.data)
+    } catch (error) {
+      setError(error)
+      // console.log(error)
+    }
+    setLoading(false)
+  }
 
-    useEffect(() => {
-        getCats();
-    }, []);
+  useEffect(() => {
+    getCats()
+  }, [])
 
-    const slugify = () =>
-        setSlug(
-            question
-                .toLowerCase()
-                .trim()
-                .replace(/[^\w\s-]/g, "")
-                .replace(/[\s_-]+/g, "-")
-                .replace(/^-+|-+$/g, "")
-        );
+  const slugify = () =>
+    setSlug(
+      question
+        .toLowerCase()
+        .trim()
+        .replace(/[^\w\s-]/g, "")
+        .replace(/[\s_-]+/g, "-")
+        .replace(/^-+|-+$/g, "")
+    )
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        setAnswer("");
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    setAnswer("")
 
-        // const variables = {
-        //   answer: answer,
-        // }
-        // if (tag.length === 0) {
-        //   setErrors((prev) => ({
-        //     ...prev,
-        //     tag: "Masukkan setidaknya satu tag",
-        //   }))
-        // }
-        // if (tag.length > 0) {
-        //   console.log(tag)
-        //   // submit form
-        // }
+    // const variables = {
+    //   answer: answer,
+    // }
+    // if (tag.length === 0) {
+    //   setErrors((prev) => ({
+    //     ...prev,
+    //     tag: "Masukkan setidaknya satu tag",
+    //   }))
+    // }
+    // if (tag.length > 0) {
+    //   console.log(tag)
+    //   // submit form
+    // }
 
-        try {
-            await axios
-                .post("http://localhost:5000/api/posts", {
-                    question,
-                    slug,
-                    category,
-                    tag,
-                    answer,
-                })
-                .then(function (response) {
-                    navigate("/dashboard/faq");
-                    //setAlert
-                });
-        } catch (err) {
-            console.log(err);
-        }
-    };
+    try {
+      await axios
+        .post("http://localhost:5000/api/posts", {
+          question,
+          slug,
+          category,
+          tag,
+          answer,
+          show,
+        })
+        .then(function (response) {
+          navigate("/dashboard/faq")
+          //setAlert
+        })
+    } catch (err) {
+      console.log(err)
+    }
+  }
 
-    return (
-        <DefaultLayout>
-            <DashboardLayout>
-                {/* title */}
-                <div className="">
-                    <DashboardTitle
-                        id="create-data-title"
-                        title="Menambah Data"
-                        subTitle="FAQ"
-                    />
-                </div>
-                <form
-                    onSubmit={handleSubmit}
-                    className="w-full bg-white rounded-lg p-8 flex flex-col gap-4"
-                >
-                    {/* Pertanyaan */}
-                    <div>
-                        <InputLabel label="Pertanyaan" />
-                        <TxtInput
-                            placeholder="Tulis pertanyaan..."
-                            value={question}
-                            onChange={(e) => setQuestion(e.target.value)}
-                        />
-                    </div>
+  return (
+    <DefaultLayout>
+      <DashboardLayout>
+        {/* title */}
+        <div className="">
+          <DashboardTitle
+            id="create-data-title"
+            title="Menambah Data"
+            subTitle="FAQ"
+          />
+        </div>
+        <form
+          onSubmit={handleSubmit}
+          className="w-full bg-white rounded-lg p-8 flex flex-col gap-4"
+        >
+          {/* Pertanyaan */}
+          <div>
+            <InputLabel label="Pertanyaan" />
+            <TxtInput
+              placeholder="Tulis pertanyaan..."
+              value={question}
+              onChange={(e) => setQuestion(e.target.value)}
+            />
+          </div>
 
-                    {/* Route URL dan Kategori */}
-                    <div className="grid grid-cols-2 gap-8">
-                        {/* Route URL */}
-                        <div>
-                            <InputLabel label="Route URL" />
-                            <div className="flex relative">
-                                <TxtInput
-                                    placeholder="Generate url..."
-                                    value={slug}
-                                    onChange={(e) => setSlug(e.target.value)}
-                                />
-                                <SlugButton onClick={slugify} />
-                            </div>
-                        </div>
+          {/* Route URL dan Kategori */}
+          <div className="grid grid-cols-2 gap-8">
+            {/* Route URL */}
+            <div>
+              <InputLabel label="Route URL" />
+              <div className="flex relative">
+                <TxtInput
+                  placeholder="Generate url..."
+                  value={slug}
+                  onChange={(e) => setSlug(e.target.value)}
+                />
+                <SlugButton onClick={slugify} />
+              </div>
+            </div>
 
-                        {/* Kategori */}
-                        <div>
-                            <InputLabel label="Kategori" />
-                            {cats ? (
-                                <DropSearch
-                                    cats={cats}
-                                    setCategory={setCategory}
-                                    placeholder="Pilih kategori"
-                                />
-                            ) : null}
-                        </div>
-                    </div>
+            {/* Kategori */}
+            <div>
+              <InputLabel label="Kategori" />
+              {cats ? (
+                <DropSearch
+                  cats={cats}
+                  setCategory={setCategory}
+                  placeholder="Pilih kategori"
+                />
+              ) : null}
+            </div>
+          </div>
 
-                    {/* Tag */}
-                    <div>
-                        <InputLabel label="Tag" />
-                        <MultiInput
-                            label="Tag"
-                            id="tag"
-                            name="tag"
-                            placeholder="Tambahkan tag"
-                            onChange={changeHandler}
-                            error={errors.tag}
-                            defaultTag={tag}
-                        />
-                    </div>
+          {/* Tag */}
+          <div>
+            <InputLabel label="Tag" />
+            <MultiInput
+              label="Tag"
+              id="tag"
+              name="tag"
+              placeholder="Tambahkan tag"
+              onChange={changeHandler}
+              error={errors.tag}
+              defaultTag={tag}
+            />
+          </div>
 
-                    {/* Jawaban */}
-                    <div>
-                        <InputLabel label="Jawaban" />
-                        <QuillEditor
-                            placeholder="Tulis jawaban"
-                            onEditorChange={onEditorChange}
-                            onFilesChange={onFilesChange}
-                        />
-                    </div>
-                    <SaveBtn />
-                </form>
+          {/* Jawaban */}
+          <div>
+            <InputLabel label="Jawaban" />
+            <QuillEditor
+              placeholder="Tulis jawaban"
+              onEditorChange={onEditorChange}
+              onFilesChange={onFilesChange}
+            />
+          </div>
 
-                {/* button */}
-            </DashboardLayout>
-        </DefaultLayout>
-    );
+          {/* Tampilkan FAQ */}
+          <div className="flex flex-col items-start">
+            <InputLabel label="Tampilkan FAQ" />
+            <input
+              type="checkbox"
+              value={show}
+              onChange={() => setIsShow((current) => !current)}
+              id="check"
+              name="check"
+              className="m-1 w-4 h-4 text-teal-900 bg-gray-100 border-gray-300 rounded focus:ring-teal-50 focus:ring-2 "
+            />
+          </div>
+
+          <SaveBtn />
+        </form>
+
+        {/* button */}
+      </DashboardLayout>
+    </DefaultLayout>
+  )
 }
